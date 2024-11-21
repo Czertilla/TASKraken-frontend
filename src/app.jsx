59508e72393bf25d7a78cfd6ef4d47e29cbf932c
Czertilla <blaztermapskorsak@gmail.com>
2024-11-21 12:45:23 +0300

    feature(app) add BrowserRouter

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,21 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Register } from './pages/register';
 import { Roles } from './pages/roles';
 
 export const App = () => {
 
   return (
-    Register()
+    <div>
+      <BrowserRouter
+        future={{
+          v7_relativeSplatPath: true,
+          v7_startTransition: true,
+        }}
+      >
+        <Routes>
+          <Route path="/register" element={<Register/>}/>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 };
