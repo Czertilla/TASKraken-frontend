f68317987528d2fceeb4efa0dd4388f47019a093
Czertilla <blaztermapskorsak@gmail.com>
2024-11-22 20:50:53 +0300

    feature(components) add Result add NotFound add Forbidden add InternalError

---
+++ src/components/Result.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Button, Result } from 'antd';
+import { Link } from 'react-router-dom';
+
+export const NotFound = () => (
+  <Result
+    status="404"
+    title="404"
+    subTitle="Sorry, the page you visited does not exist."
+    extra={<Button type="primary" href={"/register"}>Back Home</Button>}
+  />
+);
+
+export const Forbidden = () => (
+  <Result
+    status="403"
+    title="403"
+    subTitle="Sorry, you are not authorized to access this page."
+    extra={<Button type="primary">Back Home</Button>}
+  />
+);
+
+export const InternalError = () => (
+    <Result
+        status="500"
+        title="500"
+        subTitle="Sorry, something went wrong."
+        extra={<Button type="primary">Back Home</Button>}
+    />
+  );
+  
