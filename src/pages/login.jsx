8a98f2a6b62876eb443834aac051172740386e95
Czertilla <blaztermapskorsak@gmail.com>
2024-11-24 17:54:34 +0300

    feature(pages) add login page

---
+++ src/pages/login.jsx
@@ -0,0 +1,28 @@
+import Card from "antd/es/card/Card";
+import { LoginForm } from "../components/LoginForm";
+import { useParams } from 'react-router-dom';
+
+export const LoginPage = () => {
+
+  const params = useParams();
+  
+  return (
+    <div>
+        <div className="grid grid-cols-1 justify-items-center mt-20 md:mt-40">
+          <Card
+            className="shadow-lg w-11/12"
+            style={{
+              maxWidth: 500
+            }}
+            title={
+              <div>
+                  <p>Вход</p>
+              </div>
+            }
+            >
+              <LoginForm/>
+          </Card>
+        </div>
+    </div>
+  );
+};
