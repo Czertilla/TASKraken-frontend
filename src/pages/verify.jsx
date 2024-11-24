a80885963a3c8a711a36ff604791eaf83e99484a
Czertilla <blaztermapskorsak@gmail.com>
2024-11-24 14:09:48 +0300

    feature(pages) add verify page

---
+++ src/pages/verify.jsx
@@ -0,0 +1,28 @@
+import Card from "antd/es/card/Card";
+import { RegisterForm } from "../components/RegisterForm";
+import { useParams } from 'react-router-dom';
+
+export const Register = () => {
+
+  const params = useParams();
+  
+  return (
+    <div>
+        <div className="grid grid-cols-1 justify-items-center">
+          <Card
+            className="shadow-lg w-11/12"
+            style={{
+              maxWidth: 800
+            }}
+            title={
+              <div>
+                  <p>Верификация</p>
+              </div>
+            }
+            >
+              <RegisterForm/>
+          </Card>
+        </div>
+    </div>
+  );
+};
