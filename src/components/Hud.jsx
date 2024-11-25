6e76ef376a0670f9630d16b4119b6eb2b0c1afc8
Czertilla <blaztermapskorsak@gmail.com>
2024-11-26 01:40:42 +0300

    feature(Hud) add top bar flex

--- src/components/Hud.jsx
+++ src/components/Hud.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   HomeOutlined,
   MenuFoldOutlined,
@@ -7,22 +7,45 @@ import {
   UserOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
-import { Button, Layout, Menu, theme } from 'antd';
+import { Button, Flex, Layout, Menu, Tabs, theme } from 'antd';
+import { getItem } from '../utils/localStorage';
+import { ThemeSwithcer } from './ThemeSwitcher';
 const { Header, Sider, Content } = Layout;
-export const Hud = () => {
+
+const siderStyle = {
+  padding: 0,
+  overflow: "auto",
+  height: "100vh",
+  position: "sticky",
+  top: 0,
+  left: 0,
+};
+
+export const Hud = (props) => {
+  const { content } = props
+  const [themeKey, setTheme] = useState(getItem("theme", "white"))
   const [collapsed, setCollapsed] = useState(false);
   const [collapserAble, setCollapserAble] = useState(true);
+  const [buttonsColor, setBottonsColor] = useState("black")
+  const themeSwitcher = ThemeSwithcer()
   const {
-    token: { colorBgContainer, borderRadiusLG },
+      token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
+  useEffect(() => {
+      setTheme(getItem("theme", "white"))
+  }, [themeSwitcher])
+  useEffect(() => {
+      setBottonsColor(themeKey === "dark" ? "white" : "black")
+  }, [themeKey])
   return (
     <Layout>
       <Sider 
         trigger={null} 
-        theme="light" 
+        theme={themeKey} 
         collapsible 
+        style={siderStyle}
         collapsed={collapsed}
-        breakpoint="lg"
+        breakpoint="sm"
         onBreakpoint={(broken) => {
           setCollapserAble(!broken)
           setCollapsed(broken)
@@ -30,7 +53,7 @@ export const Hud = () => {
       >
         <Menu
           className='my-2'
-          theme="light"
+          theme={themeKey}
           mode="inline"
           defaultSelectedKeys={['0']}
           items={[
@@ -59,22 +82,72 @@ export const Hud = () => {
       </Sider>
       <Layout>
         <Header
+          className='justify-between'
           style={{
             padding: 0,
-            background: colorBgContainer,
+            background: themeKey === "light" ? colorBgContainer: null,
+            position: 'sticky',
+            top: 0,
+            zIndex: 1,
+            width: '100%',
+            display: 'flex',
+            alignItems: 'center',
           }}
         >
-          <Button
+          {collapserAble ? <Button
+            className='flex my-auto'
             type="text"
             disabled={!collapserAble}
-            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            icon={
+                collapsed 
+                ? <MenuUnfoldOutlined style={{color: buttonsColor}}/> 
+                : <MenuFoldOutlined style={{color: buttonsColor}}/>}
             onClick={() => setCollapsed(!collapsed)}
             style={{
               fontSize: '16px',
               width: 64,
               height: 64,
             }}
-          />
+          /> : null}
+          <Flex
+            // className="overflow-x-scroll"
+            style={{
+              overflowX: "scroll",
+              scrollbarGutter: "stable",
+              scrollbarWidth: "none"
+            }}
+          >
+            <Menu
+            theme={themeKey}
+            mode="horizontal"
+            defaultSelectedKeys={['0']}
+            items={[
+                {
+                  key: '0',
+                  icon: <HomeOutlined />,
+                  label: "home"
+                },
+                {
+                  key: '1',
+                  icon: <UserOutlined />,
+                  label: 'nav 1',
+                },
+                {
+                  key: '2',
+                  icon: <VideoCameraOutlined />,
+                  label: 'nav 2',
+                },
+                {
+                  key: '3',
+                  icon: <UploadOutlined />,
+                  label: 'nav 3',
+                },
+              ]}
+            />
+          </Flex>
+          
+          
+            {themeSwitcher}
         </Header>
         <Content
           style={{
@@ -85,7 +158,7 @@ export const Hud = () => {
             borderRadius: borderRadiusLG,
           }}
         >
-          Content
+          {content ? content : "content"}
         </Content>
       </Layout>
     </Layout>
