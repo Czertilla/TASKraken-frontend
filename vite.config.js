e7726461bf7a05b63398c1b1743562bdfa618d3f
Czertilla <blaztermapskorsak@gmail.com>
2024-12-01 15:41:34 +0300

    chore(config) add svgr

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite'
 import preact from '@preact/preset-vite'
 import tailwindcss from 'tailwindcss'
+import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact()],
+  plugins: [
+    preact(),
+    svgr({ svgrOptions: { icon: true } })
+  ],
   css: {
     postcss : {
       plugins: [tailwindcss()],
