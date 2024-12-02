c2e60d3ae401633ea0c26ba94b33fd3737cab3e8
Czertilla <blaztermapskorsak@gmail.com>
2024-12-02 22:09:01 +0300

    chore(Hud) remove useless comment

--- src/components/Hud.jsx
+++ src/components/Hud.jsx
@@ -133,7 +133,7 @@ export const Hud = (props) => {
               overflowX: "scroll",
               scrollbarGutter: "stable",
               whiteSpace: "nowrap",
-              scrollbarWidth: "none", // Убираем стандартный скроллбар для браузеров
+              scrollbarWidth: "none",
               msOverflowStyle: "none", // Для IE/Edge
             }}  
           >
