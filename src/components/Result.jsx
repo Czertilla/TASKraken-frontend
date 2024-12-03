cf8e2d54a8a064fbd87313aa21317394964d78a8
Czertilla <blaztermapskorsak@gmail.com>
2024-12-03 04:49:28 +0300

    style(Result)

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -14,7 +14,7 @@ export const Forbidden = () => (
   <Result
     status="403"
     title="403"
-    subTitle="Sorry, you are not authorized to access this page."
+    subTitle="Sorry, you cannot do this."
     extra={<Button type="primary">Back Home</Button>}
   />
 );
